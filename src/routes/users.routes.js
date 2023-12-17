e638eb9ee160cb6d56d2d7c5b9e2bf90b1726a8a
Eduardo Tavares <eduardotdev.21@gmail.com>
2023-12-17 19:47:37 -0300

    utilizando middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,9 +4,19 @@ const UsersController = require('../controllers/UsersController')
 
 const usersRoutes = Router()
 
+function myMiddleware(request, response, next) {
+  console.log("Você passou pelo Middleware!")
+
+  if (!request.body.isAdmin) {
+    return response.json({messsage: "user unauthorired"})
+  }
+
+  next()
+}
+
 const usersController = new UsersController()
 
-
-usersRoutes.post('/', usersController.create)
+// usersRoutes.use(myMiddleware)
+usersRoutes.post('/', myMiddleware, usersController.create)
 
 module.exports = usersRoutes
