35db5a9b6048caa39286221ed741b85d3377d7e7
Eduardo Tavares <eduardotdev.21@gmail.com>
2023-12-17 21:10:23 -0300

    tratando execoes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,22 +1,11 @@
-const { Router } = require('express')
+const { Router } = require("express")
 
-const UsersController = require('../controllers/UsersController')
+const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
-function myMiddleware(request, response, next) {
-  console.log("Você passou pelo Middleware!")
-
-  if (!request.body.isAdmin) {
-    return response.json({messsage: "user unauthorired"})
-  }
-
-  next()
-}
-
 const usersController = new UsersController()
 
-// usersRoutes.use(myMiddleware)
-usersRoutes.post('/', myMiddleware, usersController.create)
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
